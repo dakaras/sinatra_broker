47c95bba7458ebdb6c194a82d60d3015e657bacf
github username <github email address>
2018-03-23 12:54:28 -0500

    accounts controller done

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -10,5 +10,54 @@ class AccountsController < ApplicationController
     end
   end
 
+  get "/accounts/new" do
+    if !logged_in
+      redirect '/login'
+    else
+      erb :'golf_bags/new'
+    end
+  end
 
+  get "/accounts/:id/edit" do
+    if !logged_in
+      redirect '/login'
+    else
+      @account = Account.find(params[:id])
+      erb :'accounts/edit'
+    end
+  end
+
+  post "/accounts/:id" do
+    if !logged_in
+      redirect '/login'
+    else
+      @account = Account.find(params[:id])
+      unless Account.valid_params?(params)
+        redirect "/accounts/#{@account.id}/edit"
+      end
+      @account.update(params.select{|k|k=="name" || k=="category"})
+      redirect "/accounts/#{@account.id}"
+    end
+  end
+
+  get "/accounts/:id" do
+    if !logged_in
+      redirect '/login'
+    else
+      @account = Account.find(params[:id])
+      erb :'accounts/show'
+    end
+  end
+
+  post "/accounts" do
+    if !logged_in
+      redirect '/login'
+    else
+      unless Account.valid_params?(params)
+        redirect "/accounts/new"
+      end
+      Accounts.create(params)
+      redirect "/accounts"
+    end
+  end
 end
